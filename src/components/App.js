29397a387f995fea9980b946c959a2ecb82b9a09
FeelipePM <feelipe@email.com>
2022-02-24 12:06:31 -0300

    init project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to React!</h1>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+};
+
+export default App;
