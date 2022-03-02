e1aadf649ecf16cba31d0c8de69387e09f591822
FeelipePM <feelipe@email.com>
2022-03-02 18:02:41 -0300

    authorization by token

--- src/gql/apolloConfig.js
+++ src/gql/apolloConfig.js
@@ -1,8 +1,26 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import { HTTP_API_URL } from "../environment";
 
-export const client = new ApolloClient({
+const cache = new InMemoryCache();
+
+const httpLink = createHttpLink({
   uri: HTTP_API_URL,
-  cache: new InMemoryCache(),
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
 });
